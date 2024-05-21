6d96cd53212b4852ba9ef026252afa4fb5adecae
elvissierra <elvissierra3@gmail.com>
2024-05-21 16:03:16 -0500

    types and names

--- webscrape.py
+++ webscrape.py
@@ -1,17 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-#extract url
 req = requests.get("https://www.serebii.net/scarletviolet/pokemon.shtml")
-
 soup = BeautifulSoup(req.content, "html.parser")
-
 search = soup.find("table", class_="tab")
+#iterate through each <tr> tag
+    #iterate through each <a> tag
+        #if tag.get_text(strip=True) and pattern "/pokemon-sv/"
+        #if pattern "/pokemon-bw/type/"
 
-content = search.find_all("a", href=True)
+if search:
+    obj_data = []
+    rows = search.find_all("tr")
 
-names = [link.get_text(strip=True) for link in content if "/pokedex-sv/" in link['href']]
-for name in names:
-    print(name)
+    for row in rows:
+        a_tags = row.find_all("a", href=True)
+        names = [link.get_text(strip=True) for link in a_tags if "/pokedex-sv/" in link['href']]
 
+        img_tags = row.find_all("img", src=True)
+        types = [img["src"].split("/")[-1].replace(".gif", "") for img in img_tags if "/pokemon-bw/type/" in img["src"]]
 
+        print(names, types)
