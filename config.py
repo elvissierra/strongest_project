2175fce4fe6de979fb21a7a94d49598354641fd3
elvissierra <elvissierra3@gmail.com>
2024-05-22 16:18:31 -0500

    pgdb conn

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ def load_config(filename="database.ini", section="pgsql"):
     if parser.has_section(section):
         params = parser.items(section)
         for param in params:
-            config[params[0]] = params[1]
+            config[param[0]] = param[1]
     else:
         raise Exception("Section {0} not found in {1} file".format(section, filename))
     return config
