ede9dcc06f010d5d80c5d9aa82d7d95715526df6
elvissierra <elvissierra3@gmail.com>
2024-05-23 09:17:29 -0500

    scrape stats

--- webscrape.py
+++ webscrape.py
@@ -13,8 +13,9 @@ if search:
     rows = search.find_all("tr")
     
     for row in rows:
+
         a_tags = row.find_all("a", href=True)
-        names = [link.get_text(strip=True) for link in a_tags if "/pokedex-sv/" in link['href'] and link.get_text(strip=True)]
+        name = [link.get_text(strip=True) for link in a_tags if "/pokedex-sv/" in link['href'] and link.get_text(strip=True)]
 
         img_tags = row.find_all("img", src=True)
         types = []
@@ -23,11 +24,18 @@ if search:
             if "/pokedex-bw/type/" in src:
                 type_name = src.split('/')[-1].replace('.gif', '')
                 types.append(type_name)
-        
-        if names:
-            obj_data.append((names[0], types))
+
+        stat_tags = row.find_all("td", class_="fooinfo")
+        stats = []
+        for stat in stat_tags:
+            text = stat.get_text(strip=True)
+            if text.isdigit():
+                stats.append(int(text))
+
+        if name:
+            obj_data.append((name[0], types, stats))
     
-    for name, types in obj_data:
-        print(f"Pokémon: {name}, Types: {', '.join(types)}")
+    for name, types, stats in obj_data:
+        print(f"Pokémon: {name}, Types: {', '.join(types)}, Stats: {stats}")
 else:
-    print("Table with class 'tab' not found.")
+    print("Table not found.")
