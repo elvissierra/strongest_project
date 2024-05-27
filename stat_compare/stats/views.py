43ca2daf214b78d5759c44cb277dbf309ecfa6dd
elvissierra <elvissierra3@gmail.com>
2024-05-26 23:08:19 -0500

    get

--- stat_compare/stats/views.py
+++ stat_compare/stats/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from .models import Mon
 from .serializers import ObjSerializer
@@ -11,3 +12,10 @@ class ObjGetView(APIView):
         mons = Mon.objects.all()
         serializer = ObjSerializer(mons, many=True)
         return Response(serializer.data)
+    
+class ObjGetPutDelete(APIView):
+
+    def get(request, mon_id):
+        mon = get_object_or_404(Mon, id=mon_id)
+        return Response(ObjSerializer(mon, context={"request" : request}).data)
+
